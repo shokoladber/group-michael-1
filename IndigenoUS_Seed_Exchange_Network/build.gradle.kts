312048b273757c29a28157d7bf3ddf607fc07ae6
RP <rp@RPdeMacBook-Pro-2.local>
2024-01-21 11:27:02 -0600

    ruby-branch-starter

--- IndigenoUS_Seed_Exchange_Network/build.gradle.kts
+++ IndigenoUS_Seed_Exchange_Network/build.gradle.kts
@@ -1,19 +1,33 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "org.launchcode"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral()
 }
 
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
+
---
+++ IndigenoUS_Seed_Exchange_Network/src/main/java/org/example/IndigenoUSSeedExchangeNetword.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndigenoUSSeedExchangeNetword {
+    public static void main(String[] args) {
+        SpringApplication.run(IndigenoUSSeedExchangeNetword.class, args);
+    }
+}
