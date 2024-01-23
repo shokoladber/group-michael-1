cb77b4d239d3c1a575d212f6b21ef6f46cde2471
Rico Rose <r.andereck@gmail.com>
2024-01-22 18:18:29 -0600

    Working on blog, currently runs as intended

---
+++ IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/IndigenoUS_Seed_Exchange_Network/IndigenoUsSeedExchangeNetworkApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.IndigenoUS_Seed_Exchange_Network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndigenoUsSeedExchangeNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndigenoUsSeedExchangeNetworkApplication.class, args);
+	}
+
+}
