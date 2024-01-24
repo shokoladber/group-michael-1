ecb2a9d245e028c5601118f6458a99f738bba94f
Danielle <136658130+Smashhedo@users.noreply.github.com>
2024-01-23 20:49:29 -0600

    Added to HomeController

--- controllers/HomeController.java
+++ controllers/HomeController.java
@@ -1,4 +1,26 @@
 package controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.HashMap;
+
 public class HomeController {
+
+    @Controller
+    public class HomeController {
+
+        @GetMapping(value = "/")
+        public String index(Model model) {
+
+            HashMap<String, String> actionChoices = new HashMap<>();
+            actionChoices.put("search", "Search");
+
+            model.addAttribute("actions", actionChoices);
+
+            return "index";
+        }
+
 }
