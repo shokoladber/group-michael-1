c662882caa23f27372b508349b4423efe5c0ab31
Rico Rose <r.andereck@gmail.com>
2024-01-23 21:57:18 -0600

    Added /new-post, store doesn't really work. Need to fix "="

--- IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/IndigenoUS_Seed_Exchange_Network/controllers/BlogController.java
+++ IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/IndigenoUS_Seed_Exchange_Network/controllers/BlogController.java
@@ -3,23 +3,39 @@ package org.launchcode.IndigenoUS_Seed_Exchange_Network.controllers;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
 public class BlogController {
 
+    private Map<String, String> titleStore = new HashMap<>();
 
 @GetMapping
-
     public String blog(Model model) {
     Object attributeName;
     model.addAttribute("title", "ISEN Blog");
+    model.addAttribute("titles", titleStore);
         return "blog";
 
     }
 @GetMapping("/new-post")
     public String newPostForm(){
         return "newPost";
+        }
+
+@PostMapping("/new-post")
+    public String handlePostForm(Model model, @RequestParam String title, String author){
+    model.addAttribute("title", title);
+    model.addAttribute("author", author);
+    titleStore.put(title, author);
+    return "displayPost";
 }
 }
+
+
+
