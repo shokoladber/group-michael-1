860d9e34f8c4912092e257fe567e77ea8616e665
Rico Rose <r.andereck@gmail.com>
2024-01-27 19:38:36 -0600

    working on blog input portion

--- IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/IndigenoUS_Seed_Exchange_Network/controllers/BlogController.java
+++ IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/IndigenoUS_Seed_Exchange_Network/controllers/BlogController.java
@@ -1,8 +1,11 @@
 package org.launchcode.IndigenoUS_Seed_Exchange_Network.controllers;
 
+import org.launchcode.IndigenoUS_Seed_Exchange_Network.data.BlogData;
+import org.launchcode.IndigenoUS_Seed_Exchange_Network.models.Blog;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,29 +16,32 @@ import java.util.Map;
 @Controller
 public class BlogController {
 
-    private Map<String, String> titleStore = new HashMap<>();
+    private BlogData blogData = new BlogData();
 
 @GetMapping
     public String blog(Model model) {
     Object attributeName;
     model.addAttribute("title", "ISEN Blog");
-    model.addAttribute("titles", titleStore);
+    model.addAttribute("blogs", BlogData.getAll());
+
         return "blog";
 
     }
+
 @GetMapping("/new-post")
     public String newPostForm(){
         return "newPost";
         }
 
 @PostMapping("/new-post")
-    public String handlePostForm(Model model, @RequestParam String title, String author){
-    model.addAttribute("title", title);
-    model.addAttribute("author", author);
-    titleStore.put(title, author);
+    public String handlePostForm(Model model, @ModelAttribute Blog blog){
+ model.addAttribute("blog", blog);
+ blogData.add(blog);
     return "displayPost";
 }
 }
 
 
 
+//this syntax blogData.add is mocking how database work, but is exactly how to save something in a database
+//Review video maybe STL Women + WebDev Unit 2, Class 11, starting at 24:09/54:51 if confused. good info
--- IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/IndigenoUS_Seed_Exchange_Network/models/Blog.java
+++ IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/IndigenoUS_Seed_Exchange_Network/models/Blog.java
@@ -1,4 +1,54 @@
 package org.launchcode.IndigenoUS_Seed_Exchange_Network.models;
 
+import java.time.LocalDate;
+
 public class Blog {
+
+    private int id;
+    private static int nextId;
+private String title;
+private String author;
+private String content;
+private LocalDate time;
+
+    public Blog(String title, String author, String content) {
+        this.title = title;
+        this.author = author;
+        this.content = content;
+        this.id = nextId;
+        nextId++;
+        this.time=LocalDate.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getTime() {
+        return time;
+    }
 }
