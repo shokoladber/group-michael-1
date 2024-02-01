008c4d811bc2b1c8f909bfae9cc6606f9cca195a
RP <rp@RPdeMacBook-Pro-2.local>
2024-01-31 18:53:11 -0600

    application form working

---
+++ IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/MailConfiguration.java
@@ -0,0 +1,30 @@
+package org.launchcode;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("rubypsensory@gmail.com");
+        mailSender.setPassword("lcug xihv mbbk dmnx");
+
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+
+        mailSender.setJavaMailProperties(properties);
+        return mailSender;
+    }
+}
