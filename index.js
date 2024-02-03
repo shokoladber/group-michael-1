eb487c723f9393f15bfbc86b83a30a615b0578aa
Louis Sloan <elizasloan005@gmail.com>
2024-02-03 11:52:36 -0600

    cloudinary widget mostly functional, need to determine styling and get load time working

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    //not sure if React should be React.StrictMode
+    <React>
+    //App goes here, not sure when that gets created
+    </React>
+);
