d5a0b790ffce352f239b02d47775e254c1710557
Louis Sloan <elizasloan005@gmail.com>
2024-02-20 17:08:07 -0600

    Getting frontend and backend to communicate with eachother

---
+++ IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/IndigenoUS_Seed_Exchange_Network/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package org.launchcode.IndigenoUS_Seed_Exchange_Network.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String home() {
+        return "login";
+    }
+}
--- IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/IndigenoUS_Seed_Exchange_Network/controllers/UserController.java
+++ IndigenoUS_Seed_Exchange_Network/src/main/java/org/launchcode/IndigenoUS_Seed_Exchange_Network/controllers/UserController.java
@@ -1,54 +1,54 @@
-package org.launchcode.IndigenoUS_Seed_Exchange_Network.controllers;
-
-import jakarta.validation.Valid;
-import org.launchcode.IndigenoUS_Seed_Exchange_Network.data.AdminRepository;
-import org.launchcode.IndigenoUS_Seed_Exchange_Network.data.UserRepository;
-import org.launchcode.IndigenoUS_Seed_Exchange_Network.models.Admin;
-import org.launchcode.IndigenoUS_Seed_Exchange_Network.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Optional;
-
-@Controller
-public class UserController {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private AdminRepository adminRepository;
-
-    @GetMapping("/login")
-    public String showLoginForm (Model model) {
-        model.addAttribute("user", new User());
-        return "login";
-    }
-
-    @PostMapping("/login")
-    public String processLogin(@ModelAttribute @Valid User user, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "login";
-        }
-
-        Optional<Admin> admin = adminRepository.findByEmail(user.getEmail());
-        if (admin.isPresent()) {
-            return "redirect:/admin/dashboard";
-        }
-
-        Optional<User> storedUser = userRepository.findByEmail(user.getEmail());
-
-        if (storedUser.isPresent() && storedUser.get().getPassword().equals(user.getPassword())){
-            return "redirect:/home";
-        } else {
-            model.addAttribute("error", "Invalid Log in information");
-            return "login";
-        }
-    }
-}
-
+//package org.launchcode.IndigenoUS_Seed_Exchange_Network.controllers;
+//
+//import jakarta.validation.Valid;
+//import org.launchcode.IndigenoUS_Seed_Exchange_Network.data.AdminRepository;
+//import org.launchcode.IndigenoUS_Seed_Exchange_Network.data.UserRepository;
+//import org.launchcode.IndigenoUS_Seed_Exchange_Network.models.Admin;
+//import org.launchcode.IndigenoUS_Seed_Exchange_Network.models.User;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//import java.util.Optional;
+//
+//@Controller
+//public class UserController {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private AdminRepository adminRepository;
+//
+//    @GetMapping("/login")
+//    public String showLoginForm (Model model) {
+//        model.addAttribute("user", new User());
+//        return "login";
+//    }
+//
+//    @PostMapping("/login")
+//    public String processLogin(@ModelAttribute @Valid User user, BindingResult result, Model model) {
+//        if (result.hasErrors()) {
+//            return "login";
+//        }
+//
+//        Optional<Admin> admin = adminRepository.findByEmail(user.getEmail());
+//        if (admin.isPresent()) {
+//            return "redirect:/admin/dashboard";
+//        }
+//
+//        Optional<User> storedUser = userRepository.findByEmail(user.getEmail());
+//
+//        if (storedUser.isPresent() && storedUser.get().getPassword().equals(user.getPassword())){
+//            return "redirect:/home";
+//        } else {
+//            model.addAttribute("error", "Invalid Log in information");
+//            return "login";
+//        }
+//    }
+//}
+//
